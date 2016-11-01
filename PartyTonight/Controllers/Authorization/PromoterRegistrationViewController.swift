3c3a218991743f02b73064ea81e480d10f8d7685
Dekwin <dekstersniper@gmail.com>
2016-11-01 20:57:03 +0200

    placeholder style

--- PartyTonight/Controllers/Authorization/PromoterRegistrationViewController.swift
+++ PartyTonight/Controllers/Authorization/PromoterRegistrationViewController.swift
@@ -10,10 +10,22 @@ import UIKit
 
 class PromoterRegistrationViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var phoneNumberTextField: UITextField!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var billingInfoTextField: UITextField!
+    
+    @IBOutlet weak var emergencyContactTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setTextFieldInsets()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +33,20 @@ class PromoterRegistrationViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    
+    func setTextFieldInsets(){
+        nameTextField.attributedPlaceholder = NSAttributedString(string:"Name", attributes:[NSForegroundColorAttributeName: UIColor.white, NSFontAttributeName: UIFont(name: "Aguda-Regular2", size: 18.0)! ])
+        
+        phoneNumberTextField.attributedPlaceholder = NSAttributedString(string:"Phone number", attributes:[NSForegroundColorAttributeName: UIColor.white, NSFontAttributeName: UIFont(name: "Aguda-Regular2", size: 18.0)! ])
+        
+        emailTextField.attributedPlaceholder = NSAttributedString(string:"E-mail", attributes:[NSForegroundColorAttributeName: UIColor.white, NSFontAttributeName: UIFont(name: "Aguda-Regular2", size: 18.0)! ])
+        
+        billingInfoTextField.attributedPlaceholder = NSAttributedString(string:"Billing info", attributes:[NSForegroundColorAttributeName: UIColor.white, NSFontAttributeName: UIFont(name: "Aguda-Regular2", size: 18.0)! ])
+        
+        emergencyContactTextField.attributedPlaceholder = NSAttributedString(string:"Emergency contact", attributes:[NSForegroundColorAttributeName: UIColor.white, NSFontAttributeName: UIFont(name: "Aguda-Regular2", size: 18.0)! ])
+        
+        passwordTextField.attributedPlaceholder = NSAttributedString(string:"Password", attributes:[NSForegroundColorAttributeName: UIColor.white, NSFontAttributeName: UIFont(name: "Aguda-Regular2", size: 18.0)! ])
+    }
 
     /*
     // MARK: - Navigation
